24c0cd0c07ef8f42b9ca0b5bc80e40f83e272045
lxazs <1136012025@qq.com>
2019-05-31 16:37:09 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,12 @@
+class TestLogin:
+    def test_login01(self):
+        print("test_login01")
+        assert 0
+
+    def test_login02(self):
+        print("test_login02")
+        assert 1
+
+    def test_login03(self):
+        print("test_login023")
+        assert 1
